8f0775e0bfc054d84b0877103c3ba4e4b1c7400e
Meagan Eggert <meagan.eggert@gmail.com>
2024-09-14 11:38:02 -0700

    Added simulated loss

--- udp_hb_client.py
+++ udp_hb_client.py
@@ -1,8 +1,11 @@
 import socket
+import random
 from datetime import datetime
 
 HOST = "10.0.0.1"  # todo: specify the server's hostname or IP address inside the quotes
 PORT = 3737 # todo: specify the port number used by the server
+
+HB_num = 1;
 
 with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as s:
     s.connect((HOST, PORT))
@@ -11,8 +14,19 @@ with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as s:
         time = now.strftime("%H:%M:%S")
 #        time = now.strftime("%H:%M:%S:%f") # This format includes microseconds
 
-        message = "Heartbeat: " + time
-        byte_msg = message.encode('utf-8')
-        s.sendall(byte_msg)
-        data = s.recv(1024)
-        print("Received: {}".format(data.decode('utf-8')))
+        drop_prob = random.random()
+        if drop_prob > 0.3:
+            message = str(HB_num) + " " + time
+            byte_msg = message.encode('utf-8')
+            s.sendall(byte_msg)
+            data = s.recv(1024)
+            print("Received: {}".format(data.decode('utf-8')))
+        # For debugging purposes
+#        elif drop_prob <= 0.3:
+#            message = "message dropped"
+#            byte_msg = message.encode('utf-8')
+#            s.sendall(byte_msg)
+#            data = s.recv(1024)
+#            print("Received: {}".format(data.decode('utf-8')))
+
+        HB_num += 1
