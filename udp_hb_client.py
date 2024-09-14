cf61d4c3875085c663e9bf46eb5f0a7b478e9033
Meagan Eggert <meagan.eggert@gmail.com>
2024-09-14 10:07:25 -0700

    Adding part 2 files

---
+++ udp_hb_client.py
@@ -0,0 +1,17 @@
+## PROGRAM COPIED FROM PA1 TO BE MODIFIED TO SUIT PA2 # MEAGAN
+
+import socket
+
+HOST = ""  # todo: specify the server's hostname or IP address inside the quotes
+PORT = 0000 # todo: specify the port number used by the server
+
+with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as s:
+    s.connect((HOST, PORT))
+    message = "ping"
+    byte_msg = message.encode('utf-8')
+    s.sendall(byte_msg)
+        
+    data = s.recv(1024)
+    print("Received: {}".format(data.decode('utf-8')))
+        
+print("DONE")
