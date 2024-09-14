a29e25ab7b1285a1c2a3e42c4eff147aa043c699
Meagan Eggert <meagan.eggert@gmail.com>
2024-09-14 11:19:04 -0700

    Modifying udp_hb_client.py to include formatted timestamps

--- udp_hb_client.py
+++ udp_hb_client.py
@@ -1,4 +1,5 @@
 import socket
+from datetime import datetime
 
 HOST = "10.0.0.1"  # todo: specify the server's hostname or IP address inside the quotes
 PORT = 3737 # todo: specify the port number used by the server
@@ -6,7 +7,11 @@ PORT = 3737 # todo: specify the port number used by the server
 with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as s:
     s.connect((HOST, PORT))
     while True:
-        message = "Heartbeat"
+        now = datetime.now() # current date/time
+        time = now.strftime("%H:%M:%S")
+#        time = now.strftime("%H:%M:%S:%f") # This format includes microseconds
+
+        message = "Heartbeat: " + time
         byte_msg = message.encode('utf-8')
         s.sendall(byte_msg)
         data = s.recv(1024)
