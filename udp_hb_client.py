57c50ba88fb7f49ee2995ff0590d17bdb0e7637f
Meagan Eggert <meagan.eggert@gmail.com>
2024-09-14 10:24:56 -0700

    Minor changes to udp_hb_client.py

--- udp_hb_client.py
+++ udp_hb_client.py
@@ -2,8 +2,8 @@
 
 import socket
 
-HOST = ""  # todo: specify the server's hostname or IP address inside the quotes
-PORT = 0000 # todo: specify the port number used by the server
+HOST = "10.0.0.1"  # todo: specify the server's hostname or IP address inside the quotes
+PORT = 3737 # todo: specify the port number used by the server
 
 with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as s:
     s.connect((HOST, PORT))
