8fb3062f206be3152b0104df27cc7758a39d957d
HoppensTech <bhoppens@csumb.edu>
2024-09-14 14:24:19 -0700

    Implemented server's main function and additional functions.

--- udp_pinger_server.py
+++ udp_pinger_server.py
@@ -4,19 +4,85 @@
 import random
 from socket import socket, AF_INET, SOCK_DGRAM
 
-# TODO: 
+
+
 # 1. Create a UDP socket. Be sure to use SOCK_DGRAM since we are working with UDP packets
 # 2. Assign IP address and port number to socket
 # 3. Initialze a variable to count the number of pings
+s = socket(AF_INET, SOCK_DGRAM)
+HOST = "10.0.0.1"
+PORT = 2500
+MAX_PACKET_COUNT = 10
+# ***Initialized ping count within main***
+
+
+
+# Define which packets are going to be dropped
+def packet_drop_order():
+   tmpArray = []
+
+   # For 40 percent of expected packets to be recieved, determine which packets will be dropped.
+   for x in range(int(.4 * MAX_PACKET_COUNT)):
+      # Range starts at two because we don't want the first packet to be dropped.
+      rndNum = random.randint(2,MAX_PACKET_COUNT)
+
+      # Make sure there are no repeats in selected packets
+      while rndNum in tmpArray:
+         rndNum = random.randint(2,MAX_PACKET_COUNT)
+       
+      tmpArray.insert(x, rndNum)
+   print("Packets to be dropped: {}".format(tmpArray))
+   return tmpArray
+
+
+
+
+# Determine if packet should drop or not
+def packet_drop_valid(ping_count, packet_drops):
+   # If ping count matches predetermined packets to be dropped, return true
+   if ping_count in packet_drops:
+      return True
+   # Any other scenarios we will not drop packet
+   return False
+
+
+
 
 def main():
-    # TODO:
     # 1. Start an infinite loop
     # 2. Count the pings received using the previously initialized variable
     # 3. Generate a random number between 1 and 10 (inclusive)
     # 4. Receive the client packet along with the address it is coming from
     # 5. IF conditions for packet loss simulation are met, THEN consider the packet lost and do not respond
     # 6. Otherwise, the server responds
+    
+    # Set up socket and initialize ping count and packets to be dropped
+    print("Initializing")
+    s.bind((HOST, PORT))
+    ping_count = 0
+    packet_drops = packet_drop_order()
+    print("Ready")
+
+    while True:
+       # After reaching expected packet count, reset the ping count and packet drop order
+       if ping_count == MAX_PACKET_COUNT:
+          print("Resetting ping count and packet drop order")
+          ping_count = 0
+          packet_drops = packet_drop_order()
+
+       # Otherwise, keep incrementing the ping counter and recieving data from socket
+       ping_count += 1
+       data, addr = s.recvfrom(1024)
+       
+       # Determine if the server should respond to the client
+       if not data or packet_drop_valid(ping_count, packet_drops):
+          print("Packet dropped")
+       else:
+          print("Packet recieved")
+          s.sendto(data, addr)
+
+
+
 
 if __name__ == "__main__":
   main()
