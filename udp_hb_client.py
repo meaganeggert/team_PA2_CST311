018a39e2de957ed7c6344511b0eb1aaecbe3de4f
Meagan Eggert <meagan.eggert@gmail.com>
2024-09-14 10:53:44 -0700

    Slight modifications to make sure connection was working

--- udp_hb_client.py
+++ udp_hb_client.py
@@ -1,5 +1,3 @@
-## PROGRAM COPIED FROM PA1 TO BE MODIFIED TO SUIT PA2 # MEAGAN
-
 import socket
 
 HOST = "10.0.0.1"  # todo: specify the server's hostname or IP address inside the quotes
@@ -7,11 +5,9 @@ PORT = 3737 # todo: specify the port number used by the server
 
 with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as s:
     s.connect((HOST, PORT))
-    message = "ping"
+    message = "Hello World"
     byte_msg = message.encode('utf-8')
     s.sendall(byte_msg)
-        
     data = s.recv(1024)
-    print("Received: {}".format(data.decode('utf-8')))
-        
-print("DONE")
+
+print("Received: {}".format(data.decode('utf-8')))
--- udp_hb_server.py
+++ udp_hb_server.py
@@ -1,5 +1,5 @@
 import random
-from socket import socket, AF_INET, SOCK_DGRAM
+import socket
 
 
 HOST = "10.0.0.1"
@@ -7,19 +7,15 @@ PORT = 3737
 
 def main():
     with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as s:
-    s.bind((HOST, PORT))
-    print('Server listening on {}:{}'.format(HOST, PORT))
-    while True:
-        data, addr = s.recvfrom(1024)
-        if not data:
-            break
-        message = data.decode('utf-8')
-        message = message.upper()
-        print("SERVER:", message)
+        s.bind((HOST, PORT))
+        print('Server listening on {}:{}'.format(HOST, PORT))
+        while True:
+            data, addr = s.recvfrom(1024)
+            if not data:
+                break
+            s.sendto(data, addr)
+
         
-        data = message.encode('utf-8')
-        
-        s.sendto(data, addr)
 
 
 if __name__ == "__main__":
